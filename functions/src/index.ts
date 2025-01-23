5a639d983a08d33e7da5c5b86ba1bb46d39d3417
shark121 <boakyes175@gmail.com>
2025-01-22 19:27:37 -0600

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -41,28 +41,28 @@ const auth = getAuth();
 
 let counter = 0;
 
-let collectionsData: any = [];
+let eventCollectionData: any = [];
 
 function emptyCache() {
-  collectionsData.length = 0;
+  eventCollectionData.length = 0;
 }
 
 setInterval(emptyCache, 300000);
 
-exports.createBundle = onCall(async () => {
+exports.createBundle = onCall(async (data) => {
   counter = counter + 1;
 
-  if (collectionsData.length > 0) {
-    return { text: counter, event: collectionsData };
+  if (eventCollectionData.length > 0) {
+    return { text: counter, event: eventCollectionData };
   }
 
   const event = await db.collection("events/").get();
 
-  collectionsData = event.docs.map((doc) => {
+  eventCollectionData = event.docs.map((doc) => {
     return doc.data();
   });
 
-  return { text: counter, event: collectionsData };
+  return { text: counter, event: eventCollectionData };
 });
 
 exports.reIndexOnDelete = onDocumentDeleted("events/{docId}", async (event) => {
@@ -175,10 +175,14 @@ exports.addTeamMember = onCall(async (data) => {
       await db.collection("events").doc(body.eventId).get()
     ).data();
 
+    eventInfo && (eventInfo["permissions"] = body.permissions);
+
     await db
       .collection("users")
       .doc(userId)
-      .update({ events: adminStore.FieldValue.arrayUnion(eventInfo) });
+      .update({
+        events: adminStore.FieldValue.arrayUnion(eventInfo),
+      });
   });
 
   // const {teamId, teamMember} = data
@@ -189,4 +193,159 @@ exports.addTeamMember = onCall(async (data) => {
   // teamMembers.push(teamMember)
   // await teamRef.update({teamMembers})
   // return {teamMembers}
+});
+
+exports.removeTeamMember = onCall(async (data) => {});
+
+function addOrUpdate(
+  map: Map<string, number>,
+  key: string,
+  increment: number = 1
+): void {
+  map.set(key, (map.get(key) || 0) + increment);
+}
+
+
+type getStatisticsReturnType = {
+  err: string | null;
+  data: Record<string, number> | null;
+};
+
+exports.getStatistics = onCall(async (req):Promise<getStatisticsReturnType> => {
+  const { eventId, userId } = req.data;
+
+  if (!(eventId && userId))
+    return { err: "Missing eventId or userId", data: null };
+
+  const permissions = await db.collection("teams").doc(eventId).get();
+
+  const bookingsMap = new Map<string, number>();
+
+  if (permissions.exists) {
+    const permissionsData = permissions.data();
+
+    const userPermissions = permissionsData?.[userId]?.permissions;
+
+    if (userPermissions && userPermissions.canViewStats) {
+      const bookings = (
+        await db.collection("bookings").doc(eventId).get()
+      ).data();
+
+      for (let booking in bookings) {
+        // let currentBookings = bookingsMap.get(bookings[booking]) || new Map<string, number>()
+        // addOrUpdate(currentBookings, "quantity" , bookings[booking].scans)
+        // bookingsMap.set(booking, currentBookings)
+        addOrUpdate(
+          bookingsMap,
+          bookings[booking].tier,
+          bookings[booking].quantity
+        );
+      }
+
+      console.log(bookingsMap);
+      const bookingsMapToObject = Object.fromEntries(bookingsMap);
+
+      return { err: null, data: bookingsMapToObject };
+    }
+  }
+
+  return {
+    err: "User does not have permissions to view stats, ",
+    data: null,
+  };
+});
+
+
+type scanResultsType = {
+  scans: number | null;
+  quantity: number | null;
+  ticketData : Record<string, unknown> | null;
+  err?: string | null
+};
+
+exports.scanTicket = onCall(async (req): Promise<scanResultsType> =>{
+  const { eventId, userId, ticketID } = req.data;
+
+  if (!(eventId && userId && ticketID))
+    return { err: "Invalid request", scans: null, quantity: null, ticketData: null };
+
+  const permissions = await db.collection("teams").doc(eventId).get();
+
+  if (permissions.exists) {
+    const permissionsData = permissions.data();
+
+    const userPermissions = permissionsData?.[userId]?.permissions;
+
+    if (userPermissions && userPermissions.canScan) {
+      const ticketBookings = (
+        await db.collection("bookings").doc(eventId).get()
+      );
+
+      if (!ticketBookings.exists) {
+        console.log("Document does not exist in the database");
+        return { scans: null, err: "Document does not exist in the database", quantity: null, ticketData: null};
+      }
+
+      const bookingsData = ticketBookings.data();
+
+      if (bookingsData) {
+        console.log(Object.keys(bookingsData));
+      } else {
+        console.log("Bookings data is undefined");
+      }
+
+      if (bookingsData && Object.keys(bookingsData).includes(ticketID)) {
+        console.log(bookingsData, "bookings data");
+        const ticketData = bookingsData[ticketID];
+        console.log(ticketData, "ticket Data.......................");
+
+        const scans = ticketData.scans;
+
+        if(scans > 0) ticketData["scans"] = scans - 1;
+
+        await db.collection("bookings").doc(eventId).set(
+          {
+            [ticketID]: ticketData,
+          },
+          { merge: true }
+        );
+
+        return { scans, quantity : ticketData.quantity, err: null, ticketData: ticketData};
+      }
+    }
+  }
+
+  return {
+    err: "User does not have permissions to scan, ",
+    scans: null,
+    quantity: null,
+    ticketData: null
+  };
+});
+
+
+exports.updateEvent = onCall(async (req) => {
+  const { eventId, userId, data } = req.data;
+
+  if (!(eventId && userId && data))
+    return { err: new Error("Invalid request"), data: null };
+
+  const permissions = await db.collection("teams").doc(eventId).get();
+
+  if (permissions.exists) {
+    const permissionsData = permissions.data();
+
+    const userPermissions = permissionsData?.[userId]?.permissions;
+
+    if (userPermissions && userPermissions.canEdit) {
+      await db.collection("events").doc(eventId).set(data, { merge: true });
+      
+      return { err: null, data: data };
+    }
+  }
+
+  return {
+    err: new Error("User does not have permissions to edit, "),
+    data: null,
+  };
 });
